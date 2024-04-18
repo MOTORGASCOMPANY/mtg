6998d33caba68c5e6093c2162a5e5c65da5a7bf4
GustavoSistema <gf5535125@gmail.com>
2024-04-18 12:53:31 -0500

    probando

--- resources/views/livewire/editar-empleado.blade.php
+++ resources/views/livewire/editar-empleado.blade.php
@@ -1,6 +1,6 @@
 <div class="container mx-auto py-12">
     <div class="flex flex-row  justify-between items-center">
-        <h2 class="mt-8 font-bold text-lg text-indigo-600"> Documentos Del Empleado {{ $empleado->empleado->name }}</h2>
+        <h2 class="mt-8 font-bold text-lg text-indigo-600"> Documentos del Empleado {{ $empleado->empleado->name }}</h2>
         @hasanyrole('administrador|supervisor|Administrador del sistema')
             @livewire('create-documento-empleado', ['idEmpleado' => $empleado->id])
         @endhasanyrole
